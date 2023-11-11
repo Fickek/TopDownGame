d16c9a54f2e21124c50ead1592b00e2136f307a0
Daniil_Pavlov <daniil1758@gmail.com>
2023-11-12 00:01:13 +0600

    add character movement

--- ClassyClash/main.cpp
+++ ClassyClash/main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main() 
 {
@@ -7,8 +8,21 @@ int main()
 
 	InitWindow(windowWidth, windowHeight, "Top Down");
 
+	// texture map
 	Texture2D worldMap = LoadTexture("nature_tileset/WorldMap.png");
+
+	// texture character
+	Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+	Vector2 knightPos
+	{
+		(float)windowWidth / 2.0f - 4.0f * (0.5f * (float)knight.width / 6.0f),
+		(float)windowHeight / 2.0f - 4.0f * (0.5f * (float)knight.height)
+	};
 	
+
+
+	Vector2 mapPosition{ 0, 0 };
+	float speed = 10.0;
 
 	SetTargetFPS(60);
 
@@ -18,8 +32,27 @@ int main()
 		BeginDrawing();
 		ClearBackground(WHITE);
 
-		Vector2 mapPosition{ 0, 0 };
+		Vector2 direction{};
+		if(IsKeyDown(KEY_A)) direction.x -= 1.0;
+		if(IsKeyDown(KEY_D)) direction.x += 1.0;
+		if(IsKeyDown(KEY_W)) direction.y -= 1.0;
+		if(IsKeyDown(KEY_S)) direction.y += 1.0;
+
+		if(Vector2Length(direction) != 0.0)
+		{
+			// set mapPos = mapPos - direction
+			mapPosition = Vector2Subtract(mapPosition, Vector2Scale(Vector2Normalize(direction), speed));
+	
+		}
+
+		// draw the map
 		DrawTextureEx(worldMap, mapPosition, 0.0, 4.0, WHITE);
+
+		// draw the character
+		Rectangle source{ .0f,.0f, (float)knight.width / 6.f, (float)knight.height };
+		Rectangle dest{ knightPos.x, knightPos.y, 4.0f * (float)knight.width / 6.f, 4.0f * (float)knight.height };
+		DrawTexturePro(knight, source, dest, Vector2{}, .0f, WHITE);
+
 
 		EndDrawing();
 	}
